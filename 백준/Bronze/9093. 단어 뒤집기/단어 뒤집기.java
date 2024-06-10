3fb2783d14758084cf99ed46c63a4c776eb71500
박미정 <osshun21@gmail.com>
2024-06-10 17:57:54 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1460 ms, Memory: 52156 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,27 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            String sentence = sc.nextLine();
-            String[] words = sentence.split(" ");
-
-            StringBuilder result = new StringBuilder();
-            for (String word : words) {
-                String[] character = word.split("");
-                for (int j = character.length-1; j >= 0; j--) {
-                    result.append(character[j]);
-                }
-                result.append(" ");
-            }
-            System.out.println(result.toString());
-
-        }
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int count = Integer.parseInt(in.readLine());
+    for (int i = 0; i < count; i++) {
+      StringTokenizer st = new StringTokenizer(in.readLine());
+      while (st.hasMoreTokens()) {
+        System.out.println(new StringBuilder(st.nextToken()).reverse().toString());
+      }
     }
+  }
 }
