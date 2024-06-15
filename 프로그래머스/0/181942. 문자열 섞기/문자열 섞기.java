57aa401a728605f0696498bc56bf7ea6e08c0ff1
박미정 <osshun21@gmail.com>
2024-06-15 13:43:49 +0900

    [level 0] Title: 문자열 섞기, Time: 11.33 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         for (int i = 0; i < str1.length(); i++) {
-            answer += str1.toCharArray()[i];
-            answer += str2.toCharArray()[i];
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         return answer;
     }
