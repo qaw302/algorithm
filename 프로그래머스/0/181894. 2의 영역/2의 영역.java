14cf116d68ac45768d208b3a5a8d457f6119e1f3
박미정 <osshun21@gmail.com>
2024-07-11 21:07:01 +0900

    [level 0] Title: 2의 영역, Time: 1.21 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == 2) {
+                if (start == -1) {
+                    start = i;
+                    end = i;
+                } else {
+                    end = i;
+                }
+            }
+        }
+        if (start == -1 && end == -1) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
+        }
+
+        answer = new int[end-start+1];
+        System.arraycopy(arr, start, answer, 0, end-start+1);
+
+        return answer;
+    }
+}
