ced2ea4d17560dc98e4f6e0c660534f9d9363636
박미정 <osshun21@gmail.com>
2024-07-15 12:45:21 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.06 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        
+        return Arrays.copyOf(num_list, n);
+    }
+}
