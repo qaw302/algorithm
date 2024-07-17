038c5e2deaffe6800c1c6cf78c44b3729ee92cc8
박미정 <osshun21@gmail.com>
2024-07-17 13:29:32 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,11 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        String answer = "";
-        for (String str : my_string.split(" ")) {
-            if (str.length() > 0) {
-                answer += str+",";
-            }
-        }
-        return answer.split(",");
+        
+        return my_string.trim().split("[ ]+");
     }
 }
