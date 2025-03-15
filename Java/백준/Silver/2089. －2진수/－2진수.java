a30184c8a020d978624007c6ced9fc4a1273ab51
박미정 <osshun21@gmail.com>
2025-03-16 03:45:12 +0900

    [Silver I] Title: -2진수, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- Java/백준/Silver/2089. －2진수/－2진수.java
+++ Java/백준/Silver/2089. －2진수/－2진수.java
@@ -2,20 +2,25 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-
-        if(n==0) System.out.println(0);
-        else{
-            while(n!=1){
-                sb.append(Math.abs(n % -2));
-                n = (int)Math.ceil((double)n/(-2));
-            }
-            sb.append(n);
-
-            System.out.println(sb.reverse());
-        }
-
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		if(N==0) {
+			System.out.print("0");
+			return;
+		}
+		int r;
+		while(N!=0) {
+			r = N%-2;
+			if(r==0) {
+				sb.append("0");
+				N/=-2;
+			}
+			else {
+				sb.append("1");
+				N = (N-1)/-2;
+			}
+		}
+		System.out.print(sb.reverse());
+	}
 }
