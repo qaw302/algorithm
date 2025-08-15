e5ca0ee548747d07a7a93b8c471afe0c4da04612
박미정 <osshun21@gmail.com>
2025-08-15 11:01:43 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 232 ms, Memory: 19164 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<n; j++) {
+                if (j<n-i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
