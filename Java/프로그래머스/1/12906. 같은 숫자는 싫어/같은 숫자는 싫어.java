5792bf25db50ce5776a1c11d2862ce0d906a4910
박미정 <osshun21@gmail.com>
2025-09-22 12:19:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.21 ms, Memory: 111 MB -BaekjoonHub

--- Java/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ Java/프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        LinkedList<Integer> q = new LinkedList<>();
+        List<Integer> q = new ArrayList<>();
         for (int num : arr) {
-            
-            if (q.isEmpty() || q.getLast() != num) {
+            if (q.isEmpty() || !q.get(q.size()-1).equals(num)) {
                 q.add(num);
             }
         }
         
         int[] answer = new int[q.size()];
         for (int i=0; i<answer.length; i++) {
-            answer[i] = q.poll();
+            answer[i] = q.get(i);
         }
 
         return answer;
