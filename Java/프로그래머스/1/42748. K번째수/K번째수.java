c6f00b41506a5a6900e28f2d68216b74a2d930ef
박미정 <osshun21@gmail.com>
2025-09-28 11:48:35 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        for (int i=0; i<commands.length; i++) {
-            int[] command = commands[i];
-            int[] sub = Arrays.copyOfRange(array, command[0]-1, command[1]);
-            Arrays.sort(sub);
-            answer[i] = sub[command[2]-1];
+
+        for (int col=0; col<commands.length; col++) {
+            int[] command = commands[col];
+            int i = command[0];
+            int j = command[1];
+            int k = command[2];
+            
+            int[] subarr = new int[j-i+1];
+            System.arraycopy(array, i-1, subarr, 0, j-i+1);
+            List<Integer> list = new ArrayList<>();
+            for (int num:subarr){
+                list.add(num);
+            }
+            Collections.sort(list);
+            
+            answer[col] = list.get(k-1);
         }
         return answer;
     }
