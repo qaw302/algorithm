3784dc00f19fbb3900831205905e3150f3be1b0e
박미정 <osshun21@gmail.com>
2025-10-02 22:10:21 +0900

    [level 3] Title: 입국심사, Time: 92.78 ms, Memory: 90.2 MB -BaekjoonHub

--- Java/프로그래머스/3/43238. 입국심사/README.md
+++ Java/프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java#) 
 
 ### 성능 요약
 
-메모리: 98.1 MB, 시간: 49.27 ms
+메모리: 90.2 MB, 시간: 92.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 02일 21:55:18
+2025년 10월 02일 22:10:19
 
 ### 문제 설명
 
--- Java/프로그래머스/3/43238. 입국심사/입국심사.java
+++ Java/프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        Arrays.sort(times);
         long left = 1;
-        long right = (long) Arrays.stream(times).max().getAsInt() * n;
-        long answer = right;
-        
-        while (left <= right) {
+        long right = (long) times[times.length-1] * n;
+        long answer = 0;
+
+        while(left <= right) {
             long mid = (left + right) / 2;
             long processed = 0;
             
             for (int time : times) {
-                processed += mid / time;  // mid 시간 동안 처리 가능한 인원
-                if (processed >= n) break; // 이미 충분하면 중단
+                processed += mid / time;
+                if (processed >= n) {
+                    break;
+                }
             }
             
-            if (processed >= n) { 
+            if (processed >= n) {
                 answer = mid;
-                right = mid - 1; // 더 짧은 시간 탐색
+                right = mid - 1;
             } else {
-                left = mid + 1;  // 시간이 부족하니 늘려야 함
+                left = mid + 1;
             }
         }
         
