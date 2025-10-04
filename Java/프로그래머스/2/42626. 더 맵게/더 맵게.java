614e33aff1fcdaa43de8f6f6598421160a4e34b0
박미정 <osshun21@gmail.com>
2025-10-04 11:00:44 +0900

    [level 2] Title: 더 맵게, Time: 2064.93 ms, Memory: 129 MB -BaekjoonHub

--- Java/프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ Java/프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,23 +1,21 @@
 import java.util.PriorityQueue;
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> q = new PriorityQueue();
-        for (int n : scoville) {
-            q.add(n);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int s : scoville) {
+            q.add(s);
         }
+        
         int answer = 0;
-        while (!q.isEmpty() && q.peek() < K) {
+        while(!q.isEmpty() && q.peek() < K) {
             if (q.size() < 2) {
                 return -1;
             }
-            
             int min = q.poll();
             int nextMin = q.poll();
             q.add(min + (nextMin*2));
             answer++;
         }
-        
         return answer;
     }
 }
