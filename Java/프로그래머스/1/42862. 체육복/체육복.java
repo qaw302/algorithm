ac7a821e2819579f5c347d4223a64fd529fa9908
박미정 <osshun21@gmail.com>
2025-10-04 11:18:52 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

--- Java/프로그래머스/1/42862. 체육복/체육복.java
+++ Java/프로그래머스/1/42862. 체육복/체육복.java
@@ -1,29 +1,28 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n];
+        int[] cnt = new int[n];
         for (int l:lost) {
-            clothes[l-1]--;
+            cnt[l-1]--;
         }
-        for (int r:reserve) {
-            clothes[r-1]++;
+        for (int r : reserve) {
+            cnt[r-1]++;
         }
         
-        for (int i=0; i<n; i++) {
-            if (clothes[i] == -1) {
-                if (i>0 && clothes[i-1]>0) {
-                    clothes[i-1]--;
-                    clothes[i]++;
-                } else if (i<n-1 && clothes[i+1]>0) {
-                    clothes[i+1]--;
-                    clothes[i]++;
+        for(int i=0; i<n; i++) {
+            if (cnt[i] < 0) {
+                if (i !=0 && cnt[i-1] > 0) {
+                    cnt[i-1]--;
+                    cnt[i]++;
+                } else if (i != n-1 && cnt[i+1] > 0) {
+                    cnt[i+1]--;
+                    cnt[i]++;
                 }
             }
-            
         }
         
         int answer = 0;
-        for (int c : clothes) {
-            if (c >= 0) {
+        for (int num : cnt) {
+            if (num >= 0) {
                 answer++;
             }
         }
