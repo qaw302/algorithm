19aefd4131d5015c21e9b809afd254e1a9b35a06
박미정 <osshun21@gmail.com>
2025-10-04 13:02:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 55.31 ms, Memory: 113 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,33 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>(n+1);
-        for (int i=0; i<=n; i++) {
-            graph.add(new ArrayList<>());
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+        int[] dist = new int[n+1];
+        for (int i=1; i<=n; i++) {
+            graph.put(i, new HashSet<>());
         }
-        int[] distance = new int[n+1];
-        
         for(int[] e : edge) {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-        
         Queue<Integer> q = new LinkedList<>();
-        distance[1] = 1;
+        
+        dist[1] = 1;
         q.add(1);
         while(!q.isEmpty()) {
             int cur = q.poll();
-            for (int node : graph.get(cur)){
-                if(distance[node] == 0) {
-                    distance[node] = distance[cur] + 1;
+            for (int node : graph.get(cur)) {
+                if (dist[node] == 0) {
+                    dist[node] = dist[cur]+1;
                     q.add(node);
                 }
             }
         }
+        System.out.println(Arrays.toString(dist));
+        int max = 0;
+        for (int d : dist) {
+            max = Math.max(max, d);
+        }
         
-        int max = Arrays.stream(distance).max().getAsInt();
-        
-        int answer = (int)Arrays.stream(distance).filter(d->d==max).count();
+        int answer = 0;
+        for (int d : dist) {
+            if (d == max) {
+                answer ++;
+            }
+        }
         return answer;
     }
 }
